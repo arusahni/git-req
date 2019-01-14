c352ab883ded4a09b7c86206cfe5139fe404a6fc
Aru Sahni <arusahni@gmail.com>
2019-01-13 21:24:16 -0500

    Initial Rust impl.

---
+++ src/git.rs
@@ -0,0 +1,62 @@
+use std::str;
+
+use std::path::Path;
+
+use duct::cmd;
+use git2::{Config, Repository};
+use shellexpand;
+
+fn slugify_domain(domain: &str) -> String {
+    str::replace(domain, ".", "|")
+}
+
+pub fn get_repo_info(repo_field: &str) -> String {
+    let repo = Repository::open_from_env().expect("Couldn't find repository");
+    let cfg = repo.config().unwrap();
+    cfg.get_string(repo_field).unwrap()
+}
+
+pub fn get_config(field_name: &str) -> Option<String> {
+    let repo = Repository::open_from_env().expect("Couldn't find repository");
+    let cfg = repo.config().unwrap();
+    match cfg.get_string(&format!("req.{}", field_name)) {
+        Ok(val) => Some(val),
+        Err(_) => None
+    }
+}
+
+pub fn set_config(field_name: &str, value: &str) {
+    let repo = Repository::open_from_env().expect("Couldn't find repository");
+    let mut cfg = repo.config().unwrap();
+    cfg.set_str(&format!("req.{}", field_name), value).unwrap();
+}
+
+pub fn get_req_config(domain: &str, field: &str) -> Option<String> {
+    let slug = slugify_domain(domain);
+    let cfg = Config::open(&Path::new(&shellexpand::tilde("~/.gitreqconfig").to_string())).unwrap();
+    match cfg.get_string(&format!("req.{}.{}", slug, field)) {
+        Ok(val) => Some(val),
+        Err(_) => None
+    }
+}
+
+/// Set a value for the global git-req configuration
+pub fn set_req_config(domain: &str, field: &str, value: &str) {
+    let slug = slugify_domain(domain);
+    let mut cfg = Config::open(&Path::new(&shellexpand::tilde("~/.gitreqconfig").to_string())).unwrap();
+    cfg.set_str(&format!("req.{}.{}", slug, field), value).unwrap();
+}
+
+pub fn checkout_branch(branch_name: &str) -> Result<bool, String> {
+    let repo = Repository::open_from_env().expect("Couldn't find repository");
+    // let full_branch_name = format!("refs/heads/{}", branch_name);
+    if repo.revparse_single(branch_name).is_err() {
+        cmd!("git", "fetch").run().unwrap();
+        if repo.revparse_single(&format!("origin/{}", branch_name)).is_err() {
+            return Err(format!("Could not find remote branch: {}", branch_name));
+        }
+    }
+    debug!("Checking out branch!");
+    cmd!("git", "checkout", branch_name).run().unwrap();
+    Ok(true)
+}
---
+++ src/remotes.rs
@@ -0,0 +1,209 @@
+use std::fmt;
+use std::io::{stdin, stdout, Write};
+use regex::Regex;
+use reqwest;
+use reqwest::header::Headers;
+use crate::git;
+
+pub trait Remote {
+    fn get_project_id(&mut self) -> &str;
+    fn get_req_branch(&mut self, mr_id: &i64) -> String;
+}
+
+impl fmt::Display for Remote {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Remote")
+    }
+}
+
+impl fmt::Debug for Remote {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+struct GitHub {
+    id: String,
+    name: String,
+    origin: String,
+    api_root: String,
+}
+
+impl Remote for GitHub {
+    fn get_project_id(&mut self) -> &str {
+        &self.id
+    }
+
+    fn get_req_branch(&mut self, mr_id: &i64) -> String {
+        format!("pr/{}", mr_id)
+    }
+}
+
+#[derive(Debug)]
+struct GitLab {
+    id: String,
+    domain: String,
+    name: String,
+    namespace: String,
+    origin: String,
+    api_root: String,
+    api_key: String,
+}
+
+impl Remote for GitLab {
+    fn get_project_id(&mut self) -> &str {
+        if self.id.is_empty() {
+            self.id = format!("{}", query_gitlab_project_id(self).unwrap());
+        }
+        &self.id
+    }
+
+    fn get_req_branch(&mut self, mr_id: &i64) -> String {
+        query_gitlab_branch_name(self, mr_id)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GitLabProject {
+    id: i64,
+    description: String,
+    name: String,
+    path: String,
+    path_with_namespace: String,
+}
+
+fn query_gitlab_project_id(remote: &GitLab) -> Result<i64, &'static str> {
+    let client = reqwest::Client::new();
+    let url = reqwest::Url::parse_with_params(&format!("{}/projects", remote.api_root), &[("search", &remote.name), ("simple", &String::from("true"))]).unwrap();
+    let mut headers = Headers::new();
+    headers.set_raw("PRIVATE-TOKEN", remote.api_key.to_string());
+    debug!("{:?}", headers);
+    let mut resp = client.get(url)
+        .headers(headers)
+        .send()
+        .expect("failed to send request");
+    debug!("Response: {:?}", resp);
+    let buf: Vec<GitLabProject> = resp.json().expect("failed to read response");
+    debug!("{:?}", buf);
+    if buf.len() > 0 {
+        return Ok(buf[0].id);
+    }
+    return Err("No match found!");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GitLabMergeRequest {
+    id: i64,
+    iid: i64,
+    title: String,
+    target_branch: String,
+    source_branch: String,
+    sha: String,
+    web_url: String,
+}
+
+fn load_project_id() -> Option<String> {
+    match git::get_config("projectid") {
+        Some(project_id) => Some(project_id),
+        None => {
+            debug!("No project ID found");
+            None
+        }
+    }
+}
+
+fn query_gitlab_branch_name(remote: &GitLab, mr_id: &i64) -> String {
+    let client = reqwest::Client::new();
+    let url = reqwest::Url::parse(&format!("{}/projects/{}/merge_requests/{}", remote.api_root, remote.id, mr_id)).unwrap();
+    let mut headers = Headers::new();
+    headers.set_raw("PRIVATE-TOKEN", remote.api_key.to_string());
+    // debug!("{:?}", headers);
+    let mut resp = client.get(url)
+        .headers(headers)
+        .send()
+        .expect("failed to send request");
+    debug!("Response: {:?}", resp);
+    let buf: GitLabMergeRequest = resp.json().expect("failed to read response");
+    buf.source_branch
+}
+
+fn get_github_project_name(origin: &str) -> String {
+    let project_regex = Regex::new(r".*:(.*/\S+)\.git\w*$").unwrap();
+    let captures = project_regex.captures(origin).unwrap();
+    String::from(&captures[1])
+}
+
+fn get_gitlab_project_name(origin: &str) -> String {
+    let project_regex = Regex::new(r".*/(\S+)\.git$").unwrap();
+    let captures = project_regex.captures(origin).unwrap();
+    String::from(&captures[1])
+}
+
+fn get_gitlab_project_namespace(origin: &str) -> String {
+    let project_regex = Regex::new(r".*/(\S+)/\S+\.git$").unwrap();
+    let captures = project_regex.captures(origin).unwrap();
+    String::from(&captures[1])
+}
+
+pub fn get_domain(origin: &str) -> Result<&str, &'static str> {
+    let domain_regex = Regex::new(r"((http[s]?|ssh)://)?(\S+@)?(?P<domain>([^:/])+)").unwrap();
+    let captures = domain_regex.captures(origin);
+    if captures.is_none() {
+        return Err("invalid remote set")
+    }
+    Ok(captures.unwrap().name("domain").map_or("", |x| x.as_str()))
+}
+
+pub fn get_remote(origin: &str) -> Result<Box<Remote>, &'static str> {
+    let domain = match get_domain(origin) {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    Ok(match domain {
+        "github.com" => Box::new(GitHub {
+            id: get_github_project_name(origin),
+            name: get_github_project_name(origin),
+            origin: String::from(origin),
+            api_root: String::from("https://api.github.com/repos"),
+        }),
+        gitlab_domain => {
+            let mut remote = GitLab {
+                id: String::from(""),
+                domain: String::from(gitlab_domain),
+                name: get_gitlab_project_name(origin),
+                namespace: get_gitlab_project_namespace(origin),
+                origin: String::from(origin),
+                api_root: format!("https://{}/api/v4", gitlab_domain),
+                api_key: String::from(""),
+            };
+            let apikey = match git::get_req_config(&domain, "apikey") {
+                Some(key) => key,
+                None => {
+                    let mut newkey = String::new();
+                    print!("Please enter the read-only API key for {}: ", gitlab_domain);
+                    let _ = stdout().flush();
+                    stdin().read_line(&mut newkey).expect("Did not input a correct key");
+                    debug!("{}", &newkey);
+                    git::set_req_config(&domain, "apikey", &newkey.trim());
+                    String::from(newkey.trim())
+                }
+            };
+            info!("API Key: {}", &apikey);
+            remote.api_key = apikey;
+            let project_id = match load_project_id() {
+                Some(x) => x,
+                None    => {
+                    let project_id_str = remote.get_project_id();
+                    git::set_config("projectid", project_id_str);
+                    String::from(project_id_str)
+                }
+            };
+            info!("Got project ID: {}", project_id);
+            remote.id = project_id;
+            Box::new(remote)
+        }
+    })
+}
